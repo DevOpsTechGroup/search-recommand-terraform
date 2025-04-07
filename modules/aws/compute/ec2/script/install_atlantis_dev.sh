c5ee0e980426d7981e9761a4025eff302a428549
YOUNGMINKIM <Youngmin1085@gmail.com>
2025-04-08 00:42:51 +0900

    fix: atlantis shell script 수정

--- modules/aws/compute/ec2/script/install_atlantis_dev.sh
+++ modules/aws/compute/ec2/script/install_atlantis_dev.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
-set -xe  # 디버깅 + 실패 시 즉시 종료
+set -xe
 
 ########################################
 # 1. 시스템 설정 및 기본 패키지 설치
 ########################################
-hostnamectl --static set-hostname Atlantis
+sudo hostnamectl set-hostname Atlantis
 
 sudo dnf update -y
 sudo dnf install -y docker git vim unzip jq tree zip curl wget yum-utils --allowerasing
 
-sudo systemctl enable docker
-sudo systemctl start docker
+# 도커 시작 및 부팅 시 자동 실행 설정
+sudo systemctl enable --now docker
+
+# ec2-user를 docker 그룹에 추가 (sudo 없이 실행 가능하도록)
+sudo usermod -aG docker ec2-user
 
 ########################################
 # 2. Terraform 설치
@@ -23,27 +26,34 @@ terraform -version
 # 3. AWS CLI v2 설치
 ########################################
 cd /tmp
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install
+curl -sS "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip -q awscliv2.zip
+sudo ./aws/install --update
 rm -rf awscliv2.zip aws
 aws --version
 
 ########################################
-# 4. Dockerfile & Docker Image 생성 (in ec2-user 홈)
+# 4. Dockerfile & Docker Image 생성
 ########################################
-cat <<EOF > /home/ec2-user/Dockerfile
+cat <<EOF | sudo tee /home/ec2-user/Dockerfile > /dev/null
 FROM ghcr.io/runatlantis/atlantis:latest
 USER root
 RUN apk add --no-cache aws-cli
 EOF
 
-cd /home/ec2-user
-docker build -t atlantis .
+# ec2-user 권한으로 Docker 이미지 빌드
+sudo -u ec2-user bash -c "
+  cd /home/ec2-user
+  docker build -t atlantis .
+"
 
 ########################################
-# 5. Atlantis 컨테이너 실행 (IAM Role 사용)
+# 5. Atlantis 컨테이너 실행
 ########################################
+
+# 기존 컨테이너 제거
+docker rm -f atlantis || true
+
 docker run -d \
 -p 4141:4141 \
 --name atlantis \
@@ -52,4 +62,4 @@ atlantis server \
 --autoplan-modules \
 --gh-user=<github username> \
 --gh-token=<github token> \
---repo-allowlist=<github repository url>
+--repo-allowlist=<github repo url>
