98ef89308b51fff676c25922d0fb647f518076dc
YOUNGMINKIM <Youngmin1085@gmail.com>
2025-04-12 16:35:12 +0900

    fix: EC2 key pair 생성 제거
    1. key pair의 경우 콘솔에서 수동 생성 후, data 참조하는 방식으로 수정

--- modules/aws/compute/ec2/main.tf
+++ modules/aws/compute/ec2/main.tf
@@ -15,6 +15,17 @@ data "aws_ami" "amazon_ami" {
   }
 }
 
+# EC2 key pair
+data "aws_key_pair" "key_pair" {
+  for_each = var.ec2_key_pair
+
+  key_name = each.value.name
+
+  tags = merge(var.tags, {
+    Name = "${each.value.name}-${each.value.env}"
+  })
+}
+
 # EC2 instance
 resource "aws_instance" "ec2" {
   for_each = var.ec2_instance
@@ -37,7 +48,7 @@ resource "aws_instance" "ec2" {
   associate_public_ip_address = each.value.associate_public_ip_address # 퍼블릭 IP 할당 여부 지정(true면 공인 IP 부여 -> 고정 IP 아님)
   disable_api_termination     = each.value.disable_api_termination     # TRUE인 경우 콘솔/API로 삭제 불가
 
-  key_name = aws_key_pair.ec2_key_pair[each.value.key_pair_name].key_name # SSH key pair 지정
+  key_name = data.aws_key_pair.key_pair[each.value.key_pair_name].key_name # SSH key pair 지정
 
   vpc_security_group_ids = [ # 인스턴스에 지정될 보안그룹 ID 지정
     var.ec2_security_group_id[each.value.security_group_name]
@@ -67,29 +78,4 @@ resource "aws_instance" "ec2" {
   tags = merge(var.tags, {
     Name = "${each.value.instance_name}-${each.value.env}"
   })
-}
-
-# EC2 key pair Decide encryption method + generate a TLS/SSL private key
-resource "tls_private_key" "ec2_key_pair_rsa" {
-  for_each = var.ec2_key_pair
-
-  algorithm = each.value.key_pair_algorithm # RSA 알고리즘 설정
-  rsa_bits  = each.value.rsa_bits           # RSA 키 길이를 4096 bit로 설정 (4096 => 보안성 높은 설정 값, default => 2048)
-}
-
-# EC2 key pair
-resource "aws_key_pair" "ec2_key_pair" {
-  for_each = var.ec2_key_pair
-
-  key_name   = each.value.key_pair_name
-  public_key = tls_private_key.ec2_key_pair_rsa[each.key].public_key_openssh # Terraform이 생성한 RSA키의 공개 키를 가져와 EC2 SSH 키 페어로 등록
-}
-
-# EC2 key pair to save local file
-resource "local_file" "ec2_key_pair_local_file" {
-  for_each = var.ec2_key_pair
-
-  content         = tls_private_key.ec2_key_pair_rsa[each.key].private_key_pem
-  filename        = "${path.module}/${each.value.local_file_name}"
-  file_permission = each.value.local_file_permission
 }
