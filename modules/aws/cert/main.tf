367be322faaf8e55483276d7e9ae5648b37f3db8
ymkim <ymkim1085@funin.camp>
2025-05-21 13:03:05 +0900

    fix: route53 패키지 제거 및 acm 패키지에 route53 record 추가중
    1. 추가 진행중

--- modules/aws/cert/main.tf
+++ modules/aws/cert/main.tf
@@ -1,5 +1,5 @@
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/acm_certificate#example-usage
-# ACM SSL/TLS 인증서 생성 요청
+# ACM SSL/TLS 인증서 생성
 resource "aws_acm_certificate" "acm_certificate" {
   for_each = var.acm_certificate
 
@@ -15,3 +15,18 @@ resource "aws_acm_certificate" "acm_certificate" {
     Name = "${each.value.domain_name}-${each.value.env}"
   })
 }
+
+# Route53 레코드 생성
+# resource "aws_route53_record" "route53_record" {
+#   for_each = {
+#     for cert_key, cert_val in aws_acm_certificate.acm_certificate : 
+#       for dvo in cert_val.domain_validation_options :
+#   }
+
+#   zone_id         = data.aws_route53_zone.route53_zone.id
+#   name            = each.value.name
+#   type            = each.value.type
+#   ttl             = each.value.ttl
+#   records         = [each.value.record]
+#   allow_overwrite = each.value.allow_overwrite
+# }
